5bba432c189f1de3f4e35a8c304d8c21a7ba5dc6
Carlos <sparkycj328@gmail.com>
2022-01-03 12:00:34 -0600

    Completed Day 1 challenges

---
+++ Day 1/day1.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+func main() {
+	s := make([]int, 0)
+	windows := make([]int, 0)
+	s = open(s)
+	// fmt.Println(s)
+	iterateWindows(s, windows)
+}
+
+// Opens a file and reads in the numbers line by line
+func open(s []int) []int {
+	f, err := os.Open("sonar.txt")
+	if err != nil {
+		log.Println(err)
+	}
+
+	// Splits on newlines
+	scanner := bufio.NewScanner(f)
+
+	for scanner.Scan() {
+		num, err := strconv.Atoi(scanner.Text())
+		if err != nil {
+			log.Println(err)
+		}
+		s = append(s, num)
+	}
+	return s
+}
+
+// Determine how many times number increase
+func iterate(s []int) int {
+
+	// Keep track how many times the next number is bigger than the previous
+	increased := 0
+	for i, _ := range s {
+		if i <= 1996 {
+			if s[i+1] > s[i] {
+				increased += 1
+			} else {
+
+			}
+		} else {
+			break
+		}
+
+	}
+	return increased
+}
+
+func iterateWindows(s []int, windows []int) {
+
+	// Keep track how many times the next number is bigger than the previous
+	increased := 0
+	for i, _ := range s {
+		if i <= 1997 {
+			sum := s[i] + s[i+1] + s[i+2]
+			windows = append(windows, sum)
+			increased += 1
+		} else {
+			break
+		}
+
+	}
+	increased = iterate(windows)
+	fmt.Println(increased)
+}
